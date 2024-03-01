1ddaf28f27fb706e7a3356976d3cd606fef442c9
Hemant Kumar <hk6263274@gmail.com>
2024-03-02 01:15:51 +0530

    React Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,36 @@
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
+  // Use state to manage the current list of restaurants
+  const [restaurantList, setRestaurantList] = useState(resList);
+
   return (
-    <div className="body-contaner">
+    <div className="body-container">
       <div className="container">
         <div className="filter">
+          {/* Button to filter and show only top-rated restaurants */}
           <button
             className="filter-btn"
             onClick={() => {
-              console.log("Button clicked");
+              // Use filter to get only top-rated restaurants (avgRating > 4.3)
+              const topRated = restaurantList.filter(
+                (res) => res.info.avgRating > 4.3
+              );
+
+              // Update the restaurantList state with the filtered data
+              setRestaurantList(topRated);
             }}
           >
             Top Rated Restaurants
           </button>
         </div>
         <div className="row">
-          {resList.map((card) => {
-            return <RestaurantCard key={card.info.id} resData={card} />;
-          })}
+          {/* Map through the current restaurantList to display RestaurantCard components */}
+          {restaurantList.map((card) => (
+            <RestaurantCard key={card.info.id} resData={card} />
+          ))}
         </div>
       </div>
     </div>
